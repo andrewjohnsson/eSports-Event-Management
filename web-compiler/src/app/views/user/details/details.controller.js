982810a2a8f64d9dfd9fed151bbf3e33a6cee00f
dreusly <andrey16438@gmail.com>
2016-05-09 01:29:52 +0300

    Add User Details

--- web-compiler/src/app/views/user/details/details.controller.js
+++ web-compiler/src/app/views/user/details/details.controller.js
@@ -6,12 +6,36 @@
     .controller('UserDetailsController', UserDetailsController);
 
   /** @ngInject */
-  function UserDetailsController(UserService, $scope, $stateParams) {
+  function UserDetailsController(ApiService, $stateParams) {
     var vm = this;
 
-    UserService.get().then(function(response){
-      vm.user = response.users[$stateParams.id];
-      vm.team = $scope.teamsList[vm.user.teamId];
+    vm.apiService = ApiService;
+
+    vm.apiService.updateUsers().then(function(){
+      vm.apiService.usersList.forEach(function(user){
+        if (user.id == $stateParams.id){
+          vm.user = user;
+        }
+      });
+
+      vm.apiService.updateTeams().then(function(){
+        vm.apiService.teamsList.forEach(function(team){
+          if (team.id == vm.user.teamId){
+            vm.team = team;
+          }
+          if (team.userId == vm.user.id){
+            vm.managedTeam = team;
+          }
+        });
+      });
+
+      vm.apiService.updateEvents().then(function(){
+        vm.apiService.eventsList.forEach(function(event){
+          if (event.id == vm.user.eventId){
+            vm.event = event;
+          }
+        })
+      });
     });
   }
 
